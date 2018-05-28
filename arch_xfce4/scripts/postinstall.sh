fdee80fbab748428aee00f89185b34e99d789e9c
matthewlsawyer <matthewlsawyer@gmail.com>
2018-05-27 23:22:31 -0400

    Added scripts function

--- arch_xfce4/scripts/postinstall.sh
+++ arch_xfce4/scripts/postinstall.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 # Override xflock4 with our customized version
+chmod 755 ~/.bin/xflock4
 sudo ln -sf -t /usr/local/bin ~/.bin/xflock4
--- bootstrap.sh
+++ bootstrap.sh
@@ -2,45 +2,49 @@
 
 # Initializes a new system
 
+# Make sure the user has a configuration argument
+if [[ -z $1 ]]; then
+    echo "Missing an argument that specifies the configuration";
+    exit -1;
+fi
+if [[ ! -d "$1" ]]; then
+    echo "No directory found that matches the given configuration argument";
+    exit -1;
+fi
+
 # Ask for the administrator password upfront
 sudo -v
 
 # Keep-alive: update existing `sudo` time stamp until the script has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-distro=arch_xfce4
-
-if [[ -z $1 ]]; then
-    echo "Missing an argument that specifies the configuration";
-    exit -1;
-fi
-
-if [[ ! -d "$1" ]]; then
-    echo "No directory found that matches the given configuration argument";
-    exit -1;
-fi
-
 # Sync the dotfiles
 function sync() {
-	# Copy the staged dotfiles into the home directory
+	# Copy the dotfiles into the home directory
+    dotfiles="./$1/dotfiles/"
 	# Flags:
 	#  -a           archive
 	#  -v           verbose
 	#  -h           human readable
 	#  --no-perms   don't copy over file permissions
-	# rsync -avh --no-perms ./$1/dotfiles/ ~;
-    echo "No op";
+	sync -avh --no-perms $dotfiles ~;
 }
 
 # Run the scripts
 function scripts() {
-    echo "No op";
+    # Run the scripts in order
+    preinstall="./$1/scripts/preinstall.sh";
+    packages="./$1/scripts/packages.sh";
+    postinstall="./$1/scripts/postinstall.sh";
+    if [[ -f $preinstall ]]; then source $preinstall; fi
+    if [[ -f $packages ]]; then source $packages; fi
+    if [[ -f $postinstall ]]; then source $postinstall; fi
 }
 
 # Bootstrap the system
 function bootstrap() {
-    sync;
-    scripts;
+    sync $1;
+    scripts $1;
 }
 
-bootstrap;
+bootstrap $1;
